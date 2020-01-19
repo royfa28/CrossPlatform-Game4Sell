199ce8ef2e97f839deb5a03a13c4d5640f2d1664
Roy <roy.felix1609@gmail.com>
2020-01-19 16:38:30 +1100

    Make the signin and register module

--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { SignupPage } from '../signup/signup.page';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +11,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.page.scss'],
 })
 export class SigninPage implements OnInit {
+  signInForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private modal: ModalController,
+    private auth: AuthService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.signInForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  signIn() {
+    const email = this.signInForm.controls.email.value;
+    const password = this.signInForm.controls.password.value;
+    this.auth.signIn(email, password)
+      .then((response) => {
+        this.signInForm.reset();
+        this.router.navigate(['/notes']);
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  async signUp() {
+    const signUpModal = await this.modal.create({
+      component: SignupPage
+    });
+    signUpModal.onDidDismiss().then((response) => {
+      if (response.data) {
+        //handle signup response
+        const email = response.data.email;
+        const password = response.data.password;
+        this.auth.signUp(email, password)
+          .then((userData) => {
+            // sign up successful
+            this.router.navigate(['/notes']);
+          })
+          .catch((error) => {
+            // handle errors
+          })
+      }
+    })
+    await signUpModal.present();
   }
 
 }
